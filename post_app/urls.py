2d98516559a35b8f41d721900c86fdf9fd4e40fc
tubi55 <tubi55@nate.com>
2024-10-28 11:39:41 +0900

    관리자 어드민에 urls패턴 등록

--- post_app/urls.py
+++ post_app/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
 from .import views
 
-# posts 경로로 url접속시 views.py에 등록된 posts REST응답 함수 매팽
+# posts 경로로 url접속시 views.py에 등록된 posts REST응답 함수 맵핑
 urlpatterns = [
   path('posts', views.posts, name='posts')
 ]
